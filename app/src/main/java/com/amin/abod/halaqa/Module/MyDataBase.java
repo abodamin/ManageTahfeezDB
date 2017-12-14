b6281d32328a2c2d6b33485c0429d990ccf1c36e
abodamin <abodamin0@gmail.com>
2017-12-14 18:35:08 +0300

    Initial commit

---
+++ app/src/main/java/com/amin/abod/halaqa/Module/MyDataBase.java
@@ -0,0 +1,144 @@
+package com.amin.abod.halaqa.Module;
+/**
+ * Created by abody on 12/6/2017.
+ */
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.StrictMode;
+
+
+
+public class MyDataBase extends SQLiteOpenHelper{
+    public static final String DB_NAME = "SQLite.db";
+    public static final String DB_Table_Student = "Student";
+    public static final String DB_Table_Parent = "Parent";
+    public static final String DB_Table_Halaqa = "Halaqa";
+    public static final String DB_Table_Teacher = "Teacher";
+
+    public MyDataBase(Context context) {
+        super(context, DB_NAME, null, 1);
+    }
+
+    public long setData(String sName, int studentSSN , String sHifz , String sHalaqa , String sMobile , int parentSSN){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("name", sName);
+        contentValues.put("SSN", studentSSN);
+        contentValues.put("Hifz", sHifz);
+        contentValues.put("HalaqaName", sHalaqa);
+        contentValues.put("Mobile", sMobile);
+        contentValues.put("PSSN", parentSSN);
+        long r = db.insert(DB_Table_Student, null, contentValues);
+
+        return r;
+    }
+
+    public long setParentData(String parentName, int parentSSN, String parentMobile){
+        SQLiteDatabase db2 = this.getWritableDatabase();
+        ContentValues contentValues2 = new ContentValues();
+
+        contentValues2.put("Pname",parentName);
+        contentValues2.put("SSN",parentSSN);
+        contentValues2.put("Mobile",parentMobile);
+
+        long r2 = db2.insert(DB_Table_Parent , null , contentValues2);
+        return r2;
+    }
+
+    public long setHalaqaData(String halaqaName, String category, int halaqaTeacherSSN){
+        SQLiteDatabase db2 = this.getWritableDatabase();
+        ContentValues contentValues2 = new ContentValues();
+
+        contentValues2.put("Hname",halaqaName);
+        contentValues2.put("category",category);
+        contentValues2.put("HTSSN",halaqaTeacherSSN);
+
+        long r2 = db2.insert(DB_Table_Halaqa , null , contentValues2);
+        return r2;
+    }
+
+    public long setTeacherDate(int teacherSSN, String teacherName, String teacherMobile){
+        SQLiteDatabase db2 = this.getWritableDatabase();
+        ContentValues contentValues2 = new ContentValues();
+
+        contentValues2.put("TSSN",teacherSSN);
+        contentValues2.put("Tname",teacherName);
+        contentValues2.put("Mobile",teacherMobile);
+
+        long r2 = db2.insert(DB_Table_Teacher , null , contentValues2);
+        return r2;
+    }
+
+    public Cursor getAllRecords(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return  db.rawQuery("select * from " + DB_Table_Student , null);
+
+    }
+
+    public Cursor getAllStudentsParents(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.rawQuery("select * from "+DB_Table_Parent, null);
+    }
+
+    public static int numOfRaws;
+    public boolean updateStudentHifz(String newHifz , int Sid){    //update data of last soura
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("Hifz",newHifz);
+        numOfRaws = db.update(DB_Table_Student,contentValues,"SSN = ?",new String[] {String.valueOf(Sid)});  //id here should be as String because of the data table
+
+        return true;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("PRAGMA foreign_keys=ON;");
+        db.execSQL("create table "+ DB_Table_Student +" ( " +       //Student
+                "  SSN INTEGER " +
+                ", name TEXT " +
+                ", Hifz TEXT " +
+                ", HalaqaName TEXT" +
+                ", Mobile TEXT" +
+                ", PSSN INTEGER" +
+                ",FOREIGN KEY(HalaqaName) REFERENCES "+DB_Table_Halaqa+"(Hname) ON DELETE CASCADE " +
+                ",FOREIGN KEY(PSSN) REFERENCES "+DB_Table_Parent+"(SSN) ON UPDATE CASCADE " +
+                ", PRIMARY KEY(SSN) )");   // student  F,L
+
+
+
+        db.execSQL("create table "+ DB_Table_Parent +" (" +     //parent
+                "SSN INTEGER ," +
+                "Pname TEXT ," +
+                "Mobile TEXT ," +
+                "PRIMARY KEY(SSN))"
+        );
+
+        db.execSQL("create table "+ DB_Table_Halaqa+" " +   //Halaqa
+                "( Hname TEXT " +   //Halaqa name
+                ",category TEXT" +  //categoru of Halaqa
+                ", HTSSN INTEGER" +  //Halaqa Teacher ID
+                ", FOREIGN KEY(HTSSN) REFERENCES "+DB_Table_Teacher+"(TSSN) " +
+                ", PRIMARY KEY(Hname) )");
+
+        db.execSQL("create table "+DB_Table_Teacher +"(" +  //teacher
+                " TSSN INTEGER" +     //teacher SSN
+                ",Tname TEXT" +     //* F,L
+                ", Mobile TEXT" +
+                ", PRIMARY KEY (TSSN) )");
+
+    }
+    //create table created_topics_table (Pid INTEGER PRIMARY KEY , FOREIGN KEY(Pid) REFERENCES DB_Table_Student(id))
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists "+ DB_Table_Student);
+        onCreate(db);
+    }
+}
+
---
+++ app/src/main/java/com/amin/abod/halaqa/mainPage.java
@@ -0,0 +1,15 @@
+package com.amin.abod.halaqa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+
+    }
+}
