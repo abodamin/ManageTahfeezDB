10c6dac511f858761394215c994029e59bcf1e81
iD4xS <t2pksa@gmail.com>
2017-12-14 21:06:36 +0300

    test

--- app/src/main/java/com/amin/abod/halaqa/mainPage.java
+++ app/src/main/java/com/amin/abod/halaqa/mainPage.java
@@ -24,6 +24,7 @@ public class mainPage extends AppCompatActivity {
             }
         });
 
+
         btnHalaqa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
