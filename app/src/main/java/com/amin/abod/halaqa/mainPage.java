9f54ae6b3610282b942f4abba41a0bda53c46047
abodamin <abodamin0@gmail.com>
2017-12-14 19:47:30 +0300

    Added Student activity

--- app/src/main/java/com/amin/abod/halaqa/mainPage.java
+++ app/src/main/java/com/amin/abod/halaqa/mainPage.java
@@ -1,7 +1,10 @@
 package com.amin.abod.halaqa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class mainPage extends AppCompatActivity {
 
@@ -9,7 +12,15 @@ public class mainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+        final Intent toStudent = new Intent(mainPage.this ,students.class);
+        Button btnStudent = (Button) findViewById(R.id.btnStudent);
 
+        btnStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toStudent);
+            }
+        });
 
     }
 }
