73630a5778793602ee88351efbcc820e56a8fc36
bly373 <bly373@gmail.com>
2017-12-15 01:00:09 +0300

    add followHifz activity and connected to student activity.hifz

---
+++ app/src/main/java/com/amin/abod/halaqa/followHifz.java
@@ -0,0 +1,16 @@
+package com.amin.abod.halaqa;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class followHifz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_follow_hifz);
+
+
+    }
+}
--- app/src/main/java/com/amin/abod/halaqa/teacher.java
+++ app/src/main/java/com/amin/abod/halaqa/teacher.java
@@ -3,6 +3,7 @@ package com.amin.abod.halaqa;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class teacher extends AppCompatActivity {
 
