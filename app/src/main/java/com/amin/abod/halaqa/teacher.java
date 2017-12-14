7c32aab7f7b012dcf881bb1a38c01924e0b57d90
iD4xS <t2pksa@gmail.com>
2017-12-15 00:31:55 +0300

    Add Teacher Layout

--- app/src/main/java/com/amin/abod/halaqa/teacher.java
+++ app/src/main/java/com/amin/abod/halaqa/teacher.java
@@ -1,5 +1,6 @@
 package com.amin.abod.halaqa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,49 @@ public class teacher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+        final Intent toAddTeacher = new Intent(teacher.this,addTeacher.class);
+        Button btnAddTeacher = (Button) findViewById(R.id.btnAddTeacher);
     }
+    /*    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+        final Intent toStudent = new Intent(mainPage.this ,students.class);
+        final Intent toHalaqat = new Intent(mainPage.this ,halaqat.class);
+        final Intent toTeacher = new Intent(mainPage.this , teacher.class);
+        final Intent toManagement = new Intent(mainPage.this , management.class);
+
+        Button btnStudent = (Button) findViewById(R.id.btnStudent);
+        Button btnHalaqa = (Button) findViewById(R.id.btnHalaqa);
+        Button btnTeacher = (Button) findViewById(R.id.btnTeacher);
+        Button btnManagement = (Button) findViewById(R.id.btnManagement);
+
+        btnStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toStudent);
+            }
+        });
+
+        btnHalaqa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toHalaqat);
+            }
+        });
+
+        btnTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(toTeacher);
+            }
+        });
+
+        btnManagement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(toManagement);
+            }
+        });
+
+    }*/
 }
