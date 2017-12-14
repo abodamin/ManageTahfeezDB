f625048cd7a1945f4bef7adaf55756702bc1f75e
abodamin <abodamin0@gmail.com>
2017-12-14 21:44:00 +0300

    test

--- app/src/main/java/com/amin/abod/halaqa/mainPage.java
+++ app/src/main/java/com/amin/abod/halaqa/mainPage.java
@@ -11,6 +11,7 @@ public class mainPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main_page);
         final Intent toStudent = new Intent(mainPage.this ,students.class);
         final Intent toHalaqat = new Intent(mainPage.this ,halaqat.class);
