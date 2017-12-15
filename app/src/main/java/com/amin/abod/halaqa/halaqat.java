db1a586fd9d232042572ebd0aed03274758b1da2
iD4xS <t2pksa@gmail.com>
2017-12-15 06:33:40 +0300

    created Halaqat sublayouts with Labels, empty layouts.

--- app/src/main/java/com/amin/abod/halaqa/halaqat.java
+++ app/src/main/java/com/amin/abod/halaqa/halaqat.java
@@ -1,7 +1,10 @@
 package com.amin.abod.halaqa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class halaqat extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class halaqat extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaqat);
+
+        final Intent toAddHalaqa = new Intent(halaqat.this,addHalaqa.class);
+        Button btnAddHalaqa = (Button) findViewById(R.id.btnAddHalaqa);
+
+        final Intent toViewHalaqat = new Intent(halaqat.this,viewHalaqat.class);
+        Button btnShowHalaqat = (Button) findViewById(R.id.btnViewHalaqat);
+
+        final Intent toEditHalaqat = new Intent(halaqat.this,editHalaqat.class);
+        Button btnEditHalaqat = (Button) findViewById(R.id.btnEditHalaqat);
+
+
+        btnAddHalaqa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toAddHalaqa);
+            }
+        });
+
+        btnShowHalaqat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toViewHalaqat);
+            }
+        });
+
+        btnEditHalaqat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toEditHalaqat);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/amin/abod/halaqa/viewHalaqat.java
@@ -0,0 +1,13 @@
+package com.amin.abod.halaqa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewHalaqat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_halaqat);
+    }
+}
