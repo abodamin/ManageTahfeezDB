a6ffdac6c6e645c2d5ebd1d091b2fdec7df616c6
bly373 <bly373@gmail.com>
2017-12-15 05:09:38 +0300

    add new student activity and new student Layout and entries

--- app/src/main/java/com/amin/abod/halaqa/addStudent.java
+++ app/src/main/java/com/amin/abod/halaqa/addStudent.java
@@ -1,7 +1,10 @@
 package com.amin.abod.halaqa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class addStudent extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class addStudent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+
+        final Intent toNewStudent = new Intent(addStudent.this,addNewStudent.class);
+        final Intent toHaveBrother = new Intent(addStudent.this,addStudentHaveBrother.class);
+
+        Button btnNewStudent = (Button) findViewById(R.id.btnNewStudent);
+        Button btnHaveBrother = (Button) findViewById(R.id.btnHaveBrother);
+
+        btnNewStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(toNewStudent);
+            }
+        });
+
+        btnHaveBrother.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(toHaveBrother);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/amin/abod/halaqa/addStudentHaveBrother.java
@@ -0,0 +1,13 @@
+package com.amin.abod.halaqa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addStudentHaveBrother extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_has_brother);
+    }
+}
