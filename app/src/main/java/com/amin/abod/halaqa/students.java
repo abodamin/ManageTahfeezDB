fe11dd57a7d2b4236ed42500849c3faa0a6433eb
bly373 <bly373@gmail.com>
2017-12-15 03:42:37 +0300

    All Students Sublayouts and labels. (empty layouts)

--- app/src/main/java/com/amin/abod/halaqa/students.java
+++ app/src/main/java/com/amin/abod/halaqa/students.java
@@ -14,13 +14,40 @@ public class students extends AppCompatActivity {
         setContentView(R.layout.activity_students);
 
         final Intent toFollowHifz = new Intent(students.this, followHifz.class);
+        final Intent toAddStudent = new Intent(students.this, addStudent.class);
+        final Intent toViewStudent = new Intent(students.this, viewStudents.class);
+        final Intent toUpdateStudent = new Intent(students.this, updateStudent.class);
 
         Button btnFollowHifz = (Button) findViewById(R.id.btnHifz);
+        Button btnAddStudent = (Button) findViewById(R.id.btnAddStudent);
+        Button btnViewStudents = (Button) findViewById(R.id.btnViewStudent);
+        Button btnUpdateStudent = (Button) findViewById(R.id.btnUpdateStudent);
 
         btnFollowHifz.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(toFollowHifz);
+            }
+        });
+
+        btnAddStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(toAddStudent);
+            }
+        });
+
+        btnViewStudents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(toViewStudent);
+            }
+        });
+
+        btnUpdateStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(toUpdateStudent);
             }
         });
     }
