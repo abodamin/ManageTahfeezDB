b4e23049e5f55fc7e7ff2f955f6d82a6d184b32d
iD4xS <t2pksa@gmail.com>
2017-12-15 06:22:23 +0300

    completed Teachers sublayouts with fields, buttons and Labels for each layout. (except view teachers)

--- app/src/main/java/com/amin/abod/halaqa/teacher.java
+++ app/src/main/java/com/amin/abod/halaqa/teacher.java
@@ -12,8 +12,15 @@ public class teacher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+
         final Intent toAddTeacher = new Intent(teacher.this,addTeacher.class);
         Button btnAddTeacher = (Button) findViewById(R.id.btnAddTeacher);
+
+        final Intent toViewTeachers = new Intent(teacher.this,viewTeachers.class);
+        Button btnViewTeachers = (Button) findViewById(R.id.btnViewTeachers);
+
+        final Intent toEditTeachers = new Intent(teacher.this,editTeacher.class);
+        Button btnEditTeacher = (Button) findViewById(R.id.btnEditTeacher);
 
 
         btnAddTeacher.setOnClickListener(new View.OnClickListener() {
@@ -22,47 +29,19 @@ public class teacher extends AppCompatActivity {
                 startActivity(toAddTeacher);
             }
         });
+
+        btnViewTeachers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toViewTeachers);
+            }
+        });
+
+        btnEditTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(toEditTeachers);
+            }
+        });
     }
-    /*    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_page);
-        final Intent toStudent = new Intent(mainPage.this ,students.class);
-        final Intent toHalaqat = new Intent(mainPage.this ,halaqat.class);
-        final Intent toTeacher = new Intent(mainPage.this , teacher.class);
-        final Intent toManagement = new Intent(mainPage.this , management.class);
-
-        Button btnStudent = (Button) findViewById(R.id.btnStudent);
-        Button btnHalaqa = (Button) findViewById(R.id.btnHalaqa);
-        Button btnTeacher = (Button) findViewById(R.id.btnTeacher);
-        Button btnManagement = (Button) findViewById(R.id.btnManagement);
-
-        btnStudent.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(toStudent);
-            }
-        });
-
-        btnHalaqa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(toHalaqat);
-            }
-        });
-
-        btnTeacher.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(toTeacher);
-            }
-        });
-
-        btnManagement.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(toManagement);
-            }
-        });
-
-    }*/
 }
