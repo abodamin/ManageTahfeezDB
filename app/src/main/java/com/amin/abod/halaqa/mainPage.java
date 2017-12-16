db8cb76c69c5108c5402fa10984aceb37263a92c
iD4xS <t2pksa@gmail.com>
2017-12-17 00:26:22 +0300

    run specific activities on first run.

--- app/src/main/java/com/amin/abod/halaqa/mainPage.java
+++ app/src/main/java/com/amin/abod/halaqa/mainPage.java
@@ -1,16 +1,47 @@
 package com.amin.abod.halaqa;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class mainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        SharedPreferences sharedPreferences = getSharedPreferences("ShaPreferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        boolean  firstTime = sharedPreferences.getBoolean("first", true);
+        if(firstTime) {
+            editor.putBoolean("first", false);
+            //For commit the changes, Use either editor.commit(); or  editor.apply();.
+            editor.commit();  /*or  */
+            /*editor.apply();*/
+            for (int i = 0; i <= 2; i++) {
+                switch (i) {
+                    case 0:
+                        Intent intent = new Intent(mainPage.this, mainPage.class);
+                        startActivity(intent);
+                        Toast.makeText(mainPage.this, " يرجى إضافة معلم حلقة واحد على الأقل ", Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        Intent halaqa = new Intent(mainPage.this, addHalaqa.class);
+                        startActivity(halaqa);
+                        Toast.makeText(mainPage.this, " يرجى إضافة حلقة واحده على الأقل ", Toast.LENGTH_LONG).show();
+                        break;
+                    case 2:
+                        Intent teacher = new Intent(mainPage.this, addTeacher.class);
+                        startActivity(teacher);
+                        break;
+                }
+            }
+        }
         setContentView(R.layout.activity_main_page);
         final Intent toStudent = new Intent(mainPage.this ,students.class);
         final Intent toHalaqat = new Intent(mainPage.this ,halaqat.class);
