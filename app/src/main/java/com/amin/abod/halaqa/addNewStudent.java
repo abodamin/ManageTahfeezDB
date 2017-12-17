b090b6a9248c9f608228138072b30a33b168e2a6
iD4xS <t2pksa@gmail.com>
2017-12-17 03:57:39 +0300

    check if fields are empty in addNewStudent. (Not completed)

--- app/src/main/java/com/amin/abod/halaqa/addNewStudent.java
+++ app/src/main/java/com/amin/abod/halaqa/addNewStudent.java
@@ -3,6 +3,7 @@ package com.amin.abod.halaqa;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -43,31 +44,70 @@ MyDataBase myDataBase = new MyDataBase(this);   //parameter in the constructor t
             @Override
             public void onClick(View v) {
                 //          *** Parent Data ***
+                int pSSN=0;
+                long result1 = 0,result3=0,result2=0,result4=0;
                 String pName = parentName.getText().toString().trim();
-                int pSSN = Integer.parseInt(parentSSN.getText().toString().trim());//taking integers must be like this as i know :)
+                String pSSNText = parentSSN.getText().toString().trim();//taking integers must be like this as i know :)
+                if (! TextUtils.isEmpty(pSSNText)) pSSN = Integer.parseInt(pSSNText);
                 String pMobile = parentMobile.getText().toString().trim();
-
-                long result1 = myDataBase.setParentData(pName, pSSN, pMobile);
-
+                if(! TextUtils.isEmpty(pName) && ! TextUtils.isEmpty(pMobile) && ! TextUtils.isEmpty(pSSNText) && result3==0) {
+                    result1 = myDataBase.setParentData(pName, pSSN, pMobile);
+                    result3=5;
+                }else if(TextUtils.isEmpty(pName)){
+                    parentName.setError("لايمكن لهذه الخانة ان تكون فارغة");
+                    Toast.makeText(addNewStudent.this, " يجب إدخال اسم ولي الأمر ", Toast.LENGTH_LONG).show();
+                    result3=-1;
+                }else if(TextUtils.isEmpty(pMobile)){
+                    parentMobile.setError("لايمكن لهذه الخانة ان تكون فارغة");
+                    Toast.makeText(addNewStudent.this, " يجب إدخال رقم جوال ولي الأمر ", Toast.LENGTH_LONG).show();
+                    result3=-1;
+                }else if(TextUtils.isEmpty(pSSNText)){
+                    parentSSN.setError("لايمكن لهذه الخانة ان تكون فارغة");
+                    Toast.makeText(addNewStudent.this, " يجب إدخال رقم هوية ولي الأمر ", Toast.LENGTH_LONG).show();
+                    result3=-1;
+                }
+                if(result3 == 5){
                 if (result1 == -1) {
                     Toast.makeText(addNewStudent.this, " ولي الامر موجود استخدم قائمة تسجيل (لديه أخ) ", Toast.LENGTH_SHORT).show();
                 } else {
                     //          ***  Getting Student Data ***
+                    int sSSN = 0;
+                    String sSSNText = studentSSN.getText().toString().trim();
                     String sName = studentName.getText().toString().trim();
-                    int sSSN = Integer.parseInt(studentSSN.getText().toString().trim());
+                    if (! TextUtils.isEmpty(sSSNText)) sSSN = Integer.parseInt(pSSNText);
                     String sHifz = studentStartHifz.getText().toString().trim();
                     String sHalaqa = halaqaSpinner.getSelectedItem().toString();
                     String sMobile = studentMobile.getText().toString().trim();
-                    int paSSN = Integer.parseInt(parentSSN.getText().toString().trim());
 
-                    long result2 = myDataBase.setStudentData(sName, sSSN, sHifz, sHalaqa, sMobile, paSSN);
+                if(! TextUtils.isEmpty(sName) && ! TextUtils.isEmpty(sHifz) && ! TextUtils.isEmpty(sSSNText) && ! TextUtils.isEmpty(sHalaqa) && ! TextUtils.isEmpty(sMobile) && result4 == 0) {
+                    result2 = myDataBase.setStudentData(sName, sSSN, sHifz, sHalaqa, sMobile, pSSN);
+                    result4 = 5;
+
+                }else if(TextUtils.isEmpty(sName)){
+                    studentName.setError("لايمكن لهذه الخانة ان تكون فارغة");
+                    Toast.makeText(addNewStudent.this, " يجب إدخال اسم ولي الأمر ", Toast.LENGTH_LONG).show();
+                    result4=-1;
+                }else if(TextUtils.isEmpty(sMobile)){
+                    studentMobile.setError("لايمكن لهذه الخانة ان تكون فارغة");
+                    Toast.makeText(addNewStudent.this, " يجب إدخال رقم جوال ولي الأمر ", Toast.LENGTH_LONG).show();
+                    result4=-1;
+                }else if(TextUtils.isEmpty(sSSNText)){
+                    studentSSN.setError("لايمكن لهذه الخانة ان تكون فارغة");
+                    Toast.makeText(addNewStudent.this, " يجب إدخال رقم هوية ولي الأمر ", Toast.LENGTH_LONG).show();
+                    result4=-1;
+                }else if(TextUtils.isEmpty(sHifz)){
+                    studentStartHifz.setError("لايمكن لهذه الخانة ان تكون فارغة");
+                    Toast.makeText(addNewStudent.this, " يجب إدخال رقم هوية ولي الأمر ", Toast.LENGTH_LONG).show();
+                    result4=-1;
+                }
+                if(result4 == 5){
                     if (result2 == -1) {
                         Toast.makeText(addNewStudent.this, " خطأ في الادخال ", Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(addNewStudent.this, " تمت اضافة الطالب بنجاح ", Toast.LENGTH_SHORT).show();
                         refreshing();
                     }
-                }
+                }}}
             }
 
         });
